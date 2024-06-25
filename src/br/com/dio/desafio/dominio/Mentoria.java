5efc1a42f9d8546f10a9a714d96c7ec9f9ce3365
Gabriel Souza <gabriel123.souza06@gmail.com>
2024-06-25 16:06:12 -0300

    Dio-POO

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,44 +2,32 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
-    private LocalDate cargaHoraria;
+public class Mentoria extends Conteudo{
 
-    public String getTitulo() {
-        return titulo;
-    }
+    private LocalDate data;
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public LocalDate getCargaHoraria() {
-        return cargaHoraria;
-    }
-
-    public void setCargaHoraria(LocalDate cargaHoraria) {
-        this.cargaHoraria = cargaHoraria;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
     }
 
     public Mentoria() {
     }
 
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", cargaHoraria=" + cargaHoraria +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
                 '}';
     }
 }
